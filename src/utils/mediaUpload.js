8fe690a18bcecc95c5235a69e9bca3571bea21aa
Hasanka Piyumal <119643446+hasankapiyumal@users.noreply.github.com>
2025-07-05 22:36:54 +0530

    create mediaUpload manger function

---
+++ src/utils/mediaUpload.js
@@ -0,0 +1,38 @@
+import { createClient } from "@supabase/supabase-js";
+import toast from "react-hot-toast"
+
+export default function uploadMediaFileHandler(file){
+return new Promise((reslove,reject)=>{
+    if(file==null||file==undefined){
+      toast.error("No file selected for upload");
+      return;
+    }
+
+    const fileName=file.name;
+    const extension =fileName.split('.').pop();
+    if(extension!="jpg"|| extension !="png" || extension !="jpeg"){
+      toast.error("Only jpg, png and jpeg files are allowed");
+      return;
+    }
+
+    const timestamp = Date.now();
+    fileName=timestamp+"."+extension;
+
+    const supabase =createClient(process.env.PUBLIC_URL, process.env.PUBLIC_Key);
+    supabase.storage.from("images").upload(fileName,file,{
+        cacheControl: "3600",
+        upsert: false
+    }).then(()=>{
+         const imageURL=supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+         console.log("File uploaded successfully:", imageURL);
+         reslove(imageURL);
+            toast.success("File uploaded successfully: ");
+
+    }).catch((error)=>{
+        console.error("Error uploading file:", error);
+        toast.error("Error uploading file: " + error.message);
+        reject(error);
+    })
+
+})
+}
